7e34c639fa8ec77f4657a47496ea8d998b2d0cf1
Abdul hameed <abdulhameed000650@gmail.com>
2024-08-23 13:49:07 +0500

    .

---
+++ src/components/Child.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        {{ msg }}
+        <br>
+        {{ "Name: " + user.name +". Email: " + user.email }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'childComponent',
+    props: {
+        msg: String,
+        user:Object
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,17 +9,60 @@
         <br />
         <button v-on:click="submit" type="button">Submit</button>
 
+        <br />
+        <label for="java">Java</label>
+        <input type="checkbox" id="java" value="java" v-model="checkedNames">
+        <label for="python">Python</label>
+        <input type="checkbox" id="python" value="python" v-model="checkedNames">
+        <label for="php">PHP</label>
+        <input type="checkbox" id="php" value="php" v-model="checkedNames">
+        <br>
+        <span>Checked Language: {{ checkedNames }}</span>
+        <br>
+
+        <label for="teacher">Teacher</label>
+        <input type="radio" name="profession" value="Teacher" v-model="who" id="teacher">
+        <label for="student">Student</label>
+        <input type="radio" name="profession" value="student" v-model="who" id="student">
+        <br>
+        <span>I am: {{who}}</span>
+        <br>
+        <br>
+        <h1 v-if="show">if condition</h1>
+        <h1 v-else>else condition</h1>
+        <button v-on:click="show = !show" type="button">Toggle</button>
+        <br>
+        <br>
+        <ul>
+            <h4>Checked Language</h4>
+            <li v-for="name in checkedNames" :key="name">{{ name }} </li>
+        </ul>
+
+        <br>
+        <br>
+        <childComponent :msg=who :user=user />
     </div>
 </template>
 
 <script>
+import childComponent from './Child.vue';
+
 export default {
     name: 'Home_Component',
-
+    components: {
+        childComponent
+    },
     data() {
         return {
             name: '',
-            email: ''
+            email: '',
+            checkedNames: [],
+            who: '',
+            show: true,
+            user: {
+                name: 'John Doe',
+                email: 'john@gmail.com'
+            }
         }
     },
     methods: {
